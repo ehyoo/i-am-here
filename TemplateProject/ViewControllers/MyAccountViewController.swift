29aa10328e1e988244562cdd013f53a0f4d5ba77
Edward <ehyoo97@gmail.com>
2015-07-25 19:10:50 -0400

    fixed double login error

--- TemplateProject/ViewControllers/MyAccountViewController.swift
+++ TemplateProject/ViewControllers/MyAccountViewController.swift
@@ -1,5 +1,5 @@
 //
-//  myAccountViewController.swift
+//  MyAccountViewController.swift
 //  i_am_here
 //
 //  Created by Edward Yoo on 7/24/15.
@@ -9,14 +9,14 @@
 import UIKit
 import Parse
 
-class myAccountViewController: UIViewController {
-
+class MyAccountViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,15 +27,15 @@ class myAccountViewController: UIViewController {
         performSegueWithIdentifier("backToLogIn", sender: self)
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
